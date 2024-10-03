93d8f925fa04f1d565e2c27cd8373ebe608eda36
approachcircle <osc-mcd@protonmail.com>
2024-10-03 14:03:29 +0100

    add log formatting

--- client/src/main/java/net/approachcircle/client/CentralTextArea.java
+++ client/src/main/java/net/approachcircle/client/CentralTextArea.java
@@ -2,11 +2,16 @@ package net.approachcircle.client;
 
 import javafx.scene.control.TextArea;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class CentralTextArea {
     private static CentralTextArea instance;
     private final TextArea textArea;
+    private final SimpleDateFormat timeFormat;
 
     private CentralTextArea() {
+        timeFormat = new SimpleDateFormat("HH:mm:ss");
         textArea = new TextArea();
         textArea.setEditable(false);
     }
@@ -19,12 +24,23 @@ public class CentralTextArea {
     }
 
     public void putTextLine(String text) {
-        putText(text + "\n");
+        putTextLine(text, LogSource.System);
+    }
+
+    public void putTextLine(String text, LogSource logSource) {
+        putText(text + "\n", logSource);
     }
 
     public void putText(String text) {
-        textArea.appendText(text);
+        putText(text, LogSource.System);
     }
+
+    public void putText(String text, LogSource logSource) {
+        textArea.appendText(
+                String.format("[%s %s] %s", timeFormat.format(new Date()), logSource.name().toUpperCase(), text)
+        );
+    }
+
 
     public TextArea getTextArea() {
         return textArea;
